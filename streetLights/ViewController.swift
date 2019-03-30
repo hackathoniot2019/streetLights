c17d5ea61ce8758f37e2eb2df74a1a5e6eebe861
val0904 <val0904@yandex.ru>
2019-03-30 12:07:38 +0300

    initial commit

---
+++ streetLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  streetLights
+//
+//  Created by Валентина on 30/03/2019.
+//  Copyright © 2019 Валентина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
